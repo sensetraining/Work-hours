c2783e6c6c9bbace2a6762fc43c33a1cb872978c
sensetraining <senseit202020@gmail.com>
2022-10-07 15:11:31 +0100

    .

--- SenseITThing.py
+++ SenseITThing.py
@@ -6,6 +6,7 @@ import tkinter as tk
 import tkinter.messagebox as tkm
 import datetime as tm
 from datetime import datetime, timedelta
+from calendar import monthrange
 import os
 import time
 
@@ -192,36 +193,102 @@ def weekCheck():
             yesterdayDate = datetime.strftime(datetime.now() - timedelta(i), '%a - %d/%m/%y')
             aYesterdayDate = [yesterdayDate]
             print(yesterdayDate)
-            print(sheet.cell(9,1).value)
             print(i)
             if sheet.cell(9,1).value == yesterdayDate:
-                print(i)
                 i-=1
                 while i != 0:
                     yesterdayDate = datetime.strftime(datetime.now() - timedelta(i), '%a - %d/%m/%y')
                     aYesterdayDate = [yesterdayDate]
-                    yesterdayDay = datetime.strftime(datetime.now() - timedelta(i), '%a')
+                    yesterdayDay = datetime.strftime(datetime.now() - timedelta(i+1), '%a')
                     yesterdayMonth = datetime.strftime(datetime.now() - timedelta(i), '%d')
                     
-                    sheet.insert_row(aYesterdayDate,9)
                     print(yesterdayDate)
-
+                    
                     print(yesterdayDay)
                     if yesterdayDay == "Sun":
                         print("It's Sunday")
-                        weekTotal = ["Week Total","=SUM(B10:B16)","=SUM(C10:C16)","=SUM(D10:D16)","=SUM(E10:E16)","=SUM(F10:F16)","=SUM(G10:G16)","=SUM(H10:H16)","=SUM(I10:I16)","=SUM(J10:J16)","=SUM(K10:K16)","=SUM(L10:L16)","=SUM(M10:M16)","=SUM(N10:N16)","=SUM(O10:O16)","=SUM(P10:P16)","=SUM(Q10:Q16)","=SUM(R10:R16)","=SUM(S10:S16)","=SUM(T10:T16)","=SUM(U10:U16)","=SUM(V10:V16)","=SUM(W10:W16)","=SUM(X10:X16)","=SUM(Y10:Y16)"]
-                        sheet.insert_row(weekTotal,9,value_input_option="USER_ENTERED")
+                        addSUM1 = [10,11,12,13,14,15,16]
+                        for j in range (7):
+                            print(j)
+                            if sheet.cell(9+j,1).value == "Month Total":
+                                print("Uh oh")
+                                
+                                for k in range (j,7):
+                                    addSUM1[k] = addSUM1[k] + 1
+                                    print(k)
+                                    print(addSUM1)
+                                addSUM2 = [f"B{addSUM1[0]}+",f"B{addSUM1[1]}+",f"B{addSUM1[2]}+",f"B{addSUM1[3]}+",f"B{addSUM1[4]}+",f"B{addSUM1[5]}+",f"B{addSUM1[6]}"]
+                                addSUM = "".join(addSUM2)
+                                print(addSUM)
 
+
+                        print(f"=SUM(B{addSUM1[0]}+B{addSUM1[1]}+B{addSUM1[2]}+B{addSUM1[3]}+B{addSUM1[4]}+B{addSUM1[5]}+B{addSUM1[6]})")
+                        weekTotal = ["Week Total",
+                        f"=SUM(B{addSUM1[0]}+B{addSUM1[1]}+B{addSUM1[2]}+B{addSUM1[3]}+B{addSUM1[4]}+B{addSUM1[5]}+B{addSUM1[6]})",
+                        f"=SUM(C{addSUM1[0]}+C{addSUM1[1]}+C{addSUM1[2]}+C{addSUM1[3]}+C{addSUM1[4]}+C{addSUM1[5]}+C{addSUM1[6]})",
+                        f"=SUM(D{addSUM1[0]}+D{addSUM1[1]}+D{addSUM1[2]}+D{addSUM1[3]}+D{addSUM1[4]}+D{addSUM1[5]}+D{addSUM1[6]})",
+                        f"=SUM(E{addSUM1[0]}+E{addSUM1[1]}+E{addSUM1[2]}+E{addSUM1[3]}+E{addSUM1[4]}+E{addSUM1[5]}+E{addSUM1[6]})",
+                        f"=SUM(F{addSUM1[0]}+F{addSUM1[1]}+F{addSUM1[2]}+F{addSUM1[3]}+F{addSUM1[4]}+F{addSUM1[5]}+F{addSUM1[6]})",
+                        f"=SUM(G{addSUM1[0]}+G{addSUM1[1]}+G{addSUM1[2]}+G{addSUM1[3]}+G{addSUM1[4]}+G{addSUM1[5]}+G{addSUM1[6]})",
+                        f"=SUM(H{addSUM1[0]}+H{addSUM1[1]}+H{addSUM1[2]}+H{addSUM1[3]}+H{addSUM1[4]}+H{addSUM1[5]}+H{addSUM1[6]})",
+                        f"=SUM(I{addSUM1[0]}+I{addSUM1[1]}+I{addSUM1[2]}+I{addSUM1[3]}+I{addSUM1[4]}+I{addSUM1[5]}+I{addSUM1[6]})",
+                        f"=SUM(J{addSUM1[0]}+J{addSUM1[1]}+J{addSUM1[2]}+J{addSUM1[3]}+J{addSUM1[4]}+J{addSUM1[5]}+J{addSUM1[6]})",
+                        f"=SUM(K{addSUM1[0]}+K{addSUM1[1]}+K{addSUM1[2]}+K{addSUM1[3]}+K{addSUM1[4]}+K{addSUM1[5]}+K{addSUM1[6]})",
+                        f"=SUM(L{addSUM1[0]}+L{addSUM1[1]}+L{addSUM1[2]}+L{addSUM1[3]}+L{addSUM1[4]}+L{addSUM1[5]}+L{addSUM1[6]})",
+                        f"=SUM(M{addSUM1[0]}+M{addSUM1[1]}+M{addSUM1[2]}+M{addSUM1[3]}+M{addSUM1[4]}+M{addSUM1[5]}+M{addSUM1[6]})",
+                        f"=SUM(N{addSUM1[0]}+N{addSUM1[1]}+N{addSUM1[2]}+N{addSUM1[3]}+N{addSUM1[4]}+N{addSUM1[5]}+N{addSUM1[6]})",
+                        f"=SUM(O{addSUM1[0]}+O{addSUM1[1]}+O{addSUM1[2]}+O{addSUM1[3]}+O{addSUM1[4]}+B{addSUM1[5]}+O{addSUM1[6]})",
+                        f"=SUM(P{addSUM1[0]}+P{addSUM1[1]}+P{addSUM1[2]}+P{addSUM1[3]}+P{addSUM1[4]}+P{addSUM1[5]}+P{addSUM1[6]})",
+                        f"=SUM(Q{addSUM1[0]}+Q{addSUM1[1]}+Q{addSUM1[2]}+Q{addSUM1[3]}+Q{addSUM1[4]}+Q{addSUM1[5]}+Q{addSUM1[6]})",
+                        f"=SUM(R{addSUM1[0]}+R{addSUM1[1]}+R{addSUM1[2]}+R{addSUM1[3]}+R{addSUM1[4]}+R{addSUM1[5]}+R{addSUM1[6]})",
+                        f"=SUM(S{addSUM1[0]}+S{addSUM1[1]}+S{addSUM1[2]}+S{addSUM1[3]}+S{addSUM1[4]}+S{addSUM1[5]}+S{addSUM1[6]})",
+                        f"=SUM(T{addSUM1[0]}+T{addSUM1[1]}+T{addSUM1[2]}+T{addSUM1[3]}+T{addSUM1[4]}+T{addSUM1[5]}+T{addSUM1[6]})",
+                        f"=SUM(B{addSUM1[0]}+U{addSUM1[1]}+U{addSUM1[2]}+U{addSUM1[3]}+U{addSUM1[4]}+U{addSUM1[5]}+U{addSUM1[6]})",
+                        f"=SUM(B{addSUM1[0]}+V{addSUM1[1]}+V{addSUM1[2]}+V{addSUM1[3]}+V{addSUM1[4]}+V{addSUM1[5]}+V{addSUM1[6]})",
+                        f"=SUM(B{addSUM1[0]}+W{addSUM1[1]}+W{addSUM1[2]}+W{addSUM1[3]}+W{addSUM1[4]}+W{addSUM1[5]}+W{addSUM1[6]})",
+                        f"=SUM(B{addSUM1[0]}+X{addSUM1[1]}+X{addSUM1[2]}+X{addSUM1[3]}+X{addSUM1[4]}+X{addSUM1[5]}+X{addSUM1[6]})",
+                        f"=SUM(B{addSUM1[0]}+Y{addSUM1[1]}+Y{addSUM1[2]}+Y{addSUM1[3]}+Y{addSUM1[4]}+Y{addSUM1[5]}+Y{addSUM1[6]})",
+                        f"=SUM(B{addSUM1[0]}+Z{addSUM1[1]}+Z{addSUM1[2]}+Z{addSUM1[3]}+Z{addSUM1[4]}+Z{addSUM1[5]}+Z{addSUM1[6]})"]
+                        sheet.insert_row(weekTotal,9,value_input_option="USER_ENTERED")
                     
                     print(yesterdayMonth)
                     if yesterdayMonth == "01":
-                        print("New month")
-                        monthTotal = ["Month Total","Test"]
+                        print("End of month")
+                        lastMonth = int(datetime.strftime(datetime.now() - timedelta(i+1), '%m'))
+                        print(lastMonth)
+                        addMonth = []
+                        print(monthrange(2022, lastMonth)[1])
+                        monthLength = (monthrange(2022, lastMonth)[1])
+                        for l in range (monthLength):
+                            addMonth.append(10+monthLength)
+                        for m in range (monthLength):
+                            if sheet.cell(9+m,1).value == "Week Total":
+                                time.sleep(1)
+                                print("Oh no")
+                                for n in range(m,monthLength):
+                                    addMonth[n] = addMonth[n] + 1
+                                    print(n)
+                                    print(addMonth)
+                        
+                        monthTotal = ["Month Total"]
+                        monthRow = [""]
+                        Letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+                        for p in range (25):
+                            for o in range(monthLength):
+                                monthRow.append(f"=SUM({Letters[p+1]}{10+o}+")
+
+                        print(monthRow)
+
+
                         sheet.insert_row(monthTotal,9,value_input_option="USER_ENTERED")
+
+
+
                     time.sleep(5)
 
+                    sheet.insert_row(aYesterdayDate,9)
                     i-=1
-
+                sheet.insert_row(aYesterdayDate,9)
                 print("break")
                 break
 
@@ -239,6 +306,8 @@ currentDate = str(x.strftime("%a - %d/%m/%y"))
 #sheet.update_cell(4,1,currentDate)
 #row = ["Hello",2,"Bye"]
 date = [currentDate,]
+
+print(monthrange(2022, 10)[1])
 
 if sheet.cell(9,1).value != currentDate:
     weekCheck()
